cceb42c8c7d5244d6db7aeb1ba600fab2f8cd8a1
Hack Reactor Students <christian.monaghan@gmail.com>
2013-11-18 18:15:13 -0800

    Removed oldStep function and replaced with Dancer.prototype

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,12 +2,10 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
   var blinkyDancer = new Dancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  var oldStep = blinkyDancer.step;
-
+  
   blinkyDancer.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep(timeBetweenSteps);
+    Dancer.prototype.step.call(this,timeBetweenSteps);
 
     /* toggle() is a jQuery method to show/hide the <span> tag.
      * See http://api.jquery.com/category/effects/ for this and
